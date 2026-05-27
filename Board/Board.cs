d182450ab5e7234a850249a16d4c3a07e6be584a
Ivan Sustov <sustov05@mail.ru>
2026-05-27 18:20:05 +0300

    [Game] Fix random gems with compact symbols

--- Board/Board.cs
+++ Board/Board.cs
@@ -1,30 +1,34 @@
 public class Board {
-    const int Rows = 8;
-    const int Cols = 8;
-    readonly GemType[,] grid = new GemType[Rows, Cols];
-    private static string GetSymbol(GemType gem) {
-        return gem switch
-        {
-          GemType.Blue => "B",
-          GemType.Green => "G",
-          GemType.Red => "R",
-          GemType.Yellow => "Y",
-          _ => ".!."
+    public Board() {
+        for (int i=0; i < grid.GetLength(0); i++) {
+            for (int j=0; j < grid.GetLength(1); j++) {
+                grid[i,j] = RandomGem();
+            }
+        }
+    }
+    private GemType RandomGem(){
+    return (GemType)Random.Shared.Next(Enum.GetValues<GemType>().Length);
+    }
+    private string CompactSymbols(GemType gem){ 
+        return gem switch{
+            GemType.Blue => "B",
+            GemType.Green => "G",
+            GemType.Red => "R",
+            GemType.Yellow => "Y",
+            _ => ".!."
         };
     }
-    public Board() {
-         for (int i=0; i < grid.GetLength(0); i++) {
-            for (int j=0; j < grid.GetLength(1); j++) {
-                grid[i,j] = (GemType)Random.Shared.Next(Enum.GetValues<GemType>().Length);
-            }
-         }
-    }
+
     public void Print() {
-            for (int i=0; i < grid.GetLength(0); i++) {
+        for (int i=0; i < grid.GetLength(0); i++) {
             for (int j=0; j < grid.GetLength(1); j++) {
-                Console.Write(GetSymbol(grid[i,j]) + " ");
+                Console.Write(CompactSymbols(grid[i,j]));
             }
             Console.WriteLine();
-         }
+        }
     }
+    private static int Row = 8;
+    private static int Col = 8;
+    private GemType[,] grid = new GemType[Col,Row];
+
 }
