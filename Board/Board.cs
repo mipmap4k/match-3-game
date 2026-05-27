a4da959c477597ed0d063ef0a222e1cf0addcb78
Ivan Sustov <sustov05@mail.ru>
2026-05-27 17:56:39 +0300

    [Game] Random gems with compact symbols

--- Board/Board.cs
+++ Board/Board.cs
@@ -2,7 +2,16 @@ public class Board {
     const int Rows = 8;
     const int Cols = 8;
     readonly GemType[,] grid = new GemType[Rows, Cols];
-
+    private static string GetSymbol(GemType gem) {
+        return gem switch
+        {
+          GemType.Blue => "B",
+          GemType.Green => "G",
+          GemType.Red => "R",
+          GemType.Yellow => "Y",
+          _ => ".!."
+        };
+    }
     public Board() {
          for (int i=0; i < grid.GetLength(0); i++) {
             for (int j=0; j < grid.GetLength(1); j++) {
@@ -13,7 +22,7 @@ public class Board {
     public void Print() {
             for (int i=0; i < grid.GetLength(0); i++) {
             for (int j=0; j < grid.GetLength(1); j++) {
-                Console.Write(grid[i,j] + " ");
+                Console.Write(GetSymbol(grid[i,j]) + " ");
             }
             Console.WriteLine();
          }
